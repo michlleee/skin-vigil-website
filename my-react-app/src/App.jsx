f83a61389101e7c91d654db0ae4ccfd0d39dac14
Michelle Glebova <147517074+michlleee@users.noreply.github.com>
2025-05-17 21:08:50 +0700

    fixed spacing issues and run errors

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/home/HomePage";
-// import Upload from "";
-// import Information from "./pages/Information";
 import AboutUs from "./pages/about/AboutUsPage";
 import UploadSection from "./pages/upload/DetectUpload";
 import InformationPage from "./pages/information/InformationPage";
@@ -14,8 +12,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/upload" element={<Upload />} />
-        <Route path="/information" element={<Information />} /> */}
         <Route path="/information" element={<InformationPage/>}></Route>
         <Route path="/upload" element={<UploadSection />} />
         <Route path="/about-us" element={<AboutUs />} />
