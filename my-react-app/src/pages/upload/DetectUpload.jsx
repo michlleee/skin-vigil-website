5b053a80bb1a10c2b36a2c6ecd8c164eb46e43f5
Michelle Glebova <147517074+michlleee@users.noreply.github.com>
2025-05-23 17:24:41 +0700

    fixed typo in importing upload section

--- my-react-app/src/pages/upload/DetectUpload.jsx
+++ my-react-app/src/pages/upload/DetectUpload.jsx
@@ -1,5 +1,5 @@
 // src/components/UploadSection.jsx
-import "./uploadSectionStyles.css";
+import "./UploadSectionStyles.css";
 import { useState } from "react";
 import { FaFolderOpen } from "react-icons/fa";
 
